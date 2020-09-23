38971cb457734aa429c85968eaca2206eca00ccb
Tim Meyers <tmeyers@microsoft.com>
2020-09-23 11:21:32 -0400

    Convert timestamp. Added another check.

--- TimerTrigger1/__init__.py
+++ TimerTrigger1/__init__.py
@@ -11,11 +11,13 @@ def main(mytimer: func.TimerRequest) -> None:
 
     utc_timestamp = datetime.datetime.utcnow().replace(
         tzinfo=datetime.timezone.utc).isoformat()
+    timestamp = datetime.datetime.now()
+
 
     if mytimer.past_due:
         logging.warn('The timer is past due!')
 
-    logging.info('Python timer trigger function ran at %s', utc_timestamp)
+    logging.info('Python timer trigger function ran at %s', timestamp)
 
     sl = AuditScopeList()
     sl.initialize()
@@ -24,5 +26,11 @@ def main(mytimer: func.TimerRequest) -> None:
     scope = "DISA IL 2"
 
     flag = sl.isAtAuditScope(service,scope)
-    logging.info ("Checking [%s] at Audit Scope[%s] - Flag: %s", service, scope, flag)
+    logging.info ("Checking [%s] at Audit Scope [%s] - Flag: %s", service, scope, flag)
+    
+    service = "Azure Databricks"
+    scope = "DISA IL 5"
+
+    flag = sl.isAtAuditScope(service,scope)
+    logging.info ("Checking [%s] at Audit Scope [%s] - Flag: %s", service, scope, flag)
     
