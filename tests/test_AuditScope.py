bf965d75c617e934733d32f97c924f33541178eb
Tim Meyers <tmeyers@microsoft.com>
2021-02-05 09:46:20 -0500

    Fix tests for 20210205

--- tests/test_AuditScope.py
+++ tests/test_AuditScope.py
@@ -28,14 +28,14 @@ def test_isAtAuditScopeForCloud_noCloud(scopeList):
     ('', 'DoD CC SRG IL 2', False),
     ('', 'FedRAMP Moderate', False),
     ('', 'FedRAMP High', False),
-    ('', 'Planned 2020', False),
+    ('', 'Planned 2021', False),
 
     ('API Management', 'DoD CC SRG IL 2', True),
     ('API Management', 'FedRAMP Moderate', True),
     ('API Management', 'FedRAMP High', True),
-    ('API Management', 'Planned 2020', False),
+    ('API Management', 'Planned 2021', False),
 
-    ('Azure App Configuration', 'Planned 2020', True),
+    ('Azure Data Lake Storage', 'Planned 2021', True),
 
     ('Azure Data Box', 'FedRAMP High', True),
     
