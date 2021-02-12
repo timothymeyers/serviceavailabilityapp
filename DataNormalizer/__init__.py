9e1313baa1eb30ace0d67910d5406e5360f3ac7c
Tim Meyers <tmeyers@microsoft.com>
2021-02-12 10:59:31 -0500

    try no ...

--- DataNormalizer/__init__.py
+++ DataNormalizer/__init__.py
@@ -4,7 +4,7 @@ import azure.functions as func
 
 from ..com.auditscope import AuditScopeList
 from ..com.product_by_region import AzGovProductAvailabilty
-import ..com.data_mapping as maps
+import com.data_mapping as maps
 import json
 
 
